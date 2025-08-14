f3a7da14f77328bacde9c82a3753e5030f97da9e
ColvanVance <jose.picado8@tecnacional.edu.ni>
2025-08-14 11:54:29 -0600

    Feat:Creacion de vista edit, employee

--- resources/views/employees/edit.blade.php
+++ resources/views/employees/edit.blade.php
@@ -7,7 +7,26 @@
             <div class="card-header b-white border-0">
                 <div class="row align-items-center">
                     <div class="col-8">
-                        <h3 class="mb-0"><i class="fas fa-edit"></i> Actualizar Empleado</h3>
+                        <h3 class="mb-0"><i class="fas fa-plus-circle"></i> Actualizar Empleado</h3>
                     </div>
 
-                    <div class="col-4 text
+                    <div class="col-4 text-right">
+                        <a href="{{ route('employees.index') }}" class="btn btn-sm btn-primary">
+                            <i class="fas fa-arrow-left"></i> Regresar
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card-body container-fluid">
+                <form action="{{ route('employees.update', $employee->id) }}" method="POST">
+                    @csrf
+                    @method('PUT')
+
+                    @include('employees.form')
+                </form>
+            </div>
+        </div>
+    </div>
+
+@endsection
