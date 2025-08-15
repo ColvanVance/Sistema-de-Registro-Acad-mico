0f8aa6c9764c6969d478891a99c5728592ea4f4b
ColvanVance <jose.picado8@tecnacional.edu.ni>
2025-08-15 10:21:43 -0600

    Feat: Creacion de vista Index para Charge

--- resources/views/charges/index.blade.php
+++ resources/views/charges/index.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.panel')
-@section('title', 'Jefes/Lista')
+@section('title', 'Cargo/Lista')
 
 @section('content')
     <div class="row">
@@ -8,9 +8,9 @@
 
                 <div class="card-header border-0">
                     <div class="d-flex justify-content-between algn-items-center">
-                        <h3 class="mb-0">Jefes</h3>
+                        <h3 class="mb-0">Cargos</h3>
                         <a href="{{ route('bosses.create') }}" class="btn btn-primary">
-                            <i class="fas fa-plus"></i> Crear nuevo Jefe
+                            <i class="fas fa-plus"></i> Crear nuevo cargo
                         </a>
                     </div>
                 </div>
@@ -19,4 +19,52 @@
                     <table class="table align-items-center table-flush">
                         <thead class="thead-light">
                             <tr>
-                                th*
+                                <th scope="Nombre del cargo"></th>
+                                <th scope="Tipo de cargo"></th>
+                                <th scope="Nivel academico"></th>
+                                <th scope="Experiencia de trabajo"></th>
+                                <th scope="Requerimientos necesarios"></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+@foreach ($charges as $charge)
+                            <tr>
+                                <span class="badge badge-pill badge-primary">{{ $charge->id }}</span>
+                            </td>
+                            <td>{{ $charge->name_charge }}</td>
+                            <td>{{ $charge->type_charge }}</td>
+                            <td>{{ $charge->academic_level }}</td>
+                            <td>{{ $charge->experience_works }}</td>
+                            <td>{{ $charge->requeriments }}</td>
+
+                            <td style="withe-space: nowrap; display: align-items; center;">
+                                        <a href="{{ route('charges.show', $charge) }}" class="btn btn-primary btn-sm" style="margin-right: 5px;">
+                                           <i class="fas fa-eye"></i> Ver
+                                        <a href="{{ route('charges.edit', $charge) }}" class="btn btn-sm btn-primary" style="margin-right: 5px;">
+                                            <i class="fas fa-eye"></i> Editar
+                                        </a>
+                                        <form action="{{ route('charges.destroy', $charge->id) }}" method="POST"
+                                            style="display: inline-block; margin: 0; display: flex; align-items: center;"
+                                            onsubmit="return confirm('¿Estás seguro de eliminar este empleado? Esta accion no se puede desahacer.');">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="submit" class="btn btn-danger btn-sm">
+                                                <i class="fas fa-trash"></i> Eliminar
+                                            </button>
+                                        </form>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+
+                <div class="card-footer py-4">
+                    <nav aria-label="..." class="d-flex flex-wrap justify-content-center justify-content-ld-center">
+                        {{ $charges->links() }}
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
