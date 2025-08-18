3075727a38d9987940585327c456fd2b0388ca1e
ColvanVance <jose.picado8@tecnacional.edu.ni>
2025-08-18 10:51:42 -0600

    Feat: Creación de vista Departaments/edit

--- resources/views/departaments/edit.blade.php
+++ resources/views/departaments/edit.blade.php
@@ -1,27 +1,28 @@
 @extends('layouts.panel')
-@section('title', 'Ausencias/Crear')
+
+@section('title', 'Departamentos/Actualizar')
 
 @section('content')
+
 <div class="col-x1-12 order-x1-1">
     <div class="card bg-secondary shadow">
-
         <div class="card-header bg-white border-0">
             <div class="row align-items-center">
                 <div class="col-8">
-
-                    <h3 class="mb-0">  <i class="fas fa-plus-circle"> </i> Registrar Ausencia</h3>
+                    <h3 class="mb-0"> <i class="fas fa-plus-circle"> </i> Actualizar Departamentos</h3>
                 </div>
                 <div class="col-4 text-right">
-                    <a href="{{ route('absences.index') }}" class="btn btn-sm btn-primary">
+                    <a href="{{ route('departaments.index') }}" class="btn btn-sm btn-primary">
                         <i class="fas fa-arrow-left"></i> Volver
                     </a>
                 </div>
             </div>
         </div>
         <div class="card-body container-fluid">
-            <form action="{{ route("absences.store") }}" method="POST">
+            <form action="{{ route("departaments.update", $departaments->id) }}" method="POST">
                 @csrf
-                @include('absences.form')
+                @method('PUT')
+                @include('departaments.form')
             </form>
         </div>
     </div>
