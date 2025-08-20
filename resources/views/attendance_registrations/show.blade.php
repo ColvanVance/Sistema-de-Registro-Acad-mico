45a5e1f2463c5e11dbeaa1df6ca783db7ae7e6e0
ColvanVance <jose.picado8@tecnacional.edu.ni>
2025-08-20 11:45:53 -0600

    Feat: Creación de la vista att_registrations/show

--- resources/views/attendance_registrations/show.blade.php
+++ resources/views/attendance_registrations/show.blade.php
@@ -18,41 +18,69 @@
                 </div>
             </div>
 
-        <div class="card-body">
-            <h6 class="heading-small text-muted mb-4">Información del Departamento</h6>
-            <div class="pl-lg-4">
+            <div class="card-body">
+                <h6 class="heading-small text-muted mb-4">Información del Departamento</h6>
+                    <div class="pl-lg-4">
 
-                <div class="row">
-                    <div class="col-lg-12">
-                        <div class="form-group">
-                            <label class="form-control-label" for="hours_worked">
-                                <i class="fas fa-user"></i>Horas trabajadas
-                            </label>
-                            <p>{{ $attendance_registrations->hours_worked }}</p>
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="form-group">
+                                <label class="form-control-label" for="hours_worked">
+                                    <i class="fas fa-user"></i>Horas trabajadas
+                                </label>
+                                <p>{{ $attendance_registrations->hours_worked }}</p>
+                            </div>
                         </div>
                     </div>
-                </div>
 
-                <div class="row">
-                    <div class="col-lg-12">
-                        <div class="form-group">
-                            <label class="form-control-label" for="employees_id">
-                                <i class="fas fa-user"></i>Nombre del empleado
-                            </label>
-                            <p>{{ $attendance_registrations->employee->name }}</p>
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="form-group">
+                                <label class="form-control-label" for="employees_id">
+                                    <i class="fas fa-user"></i>Nombre del empleado
+                                </label>
+                                <p>{{ $employees_id->name }}</p>
+                            </div>
                         </div>
                     </div>
-                </div>
 
                    <div class="row">
-                    <div class="col-lg-12">
-                        <div class="form-group">
-                            <label class="form-control-label" for="description">
-                                <i class="fas fa-user"></i>Descripcion del departamento
-                            </label>
-                            <p>{{ $departaments->description }}</p>
+                        <div class="col-lg-12">
+                            <div class="form-group">
+                                <label class="form-control-label" for="charges_id">
+                                    <i class="fas fa-user"></i>Cargo del empleado
+                                </label>
+                                <p>{{ $charges_id->name_charge }}</p>
+                            </div>
                         </div>
                     </div>
+
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="form-group">
+                                <label class="form-control-label" for="incidences_id">
+                                    <i class="fas fa-user"></i>Incidencias del empleado
+                                </label>
+                                <p>{{ $incidences_id->type }}</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="form-group">
+                                <label class="form-control-label" for="incidences_id">
+                                    <i class="fas fa-user"></i>Estado de incidencia
+                                </label>
+                                <p>{{ $incidences_id->status }}</p>
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
+            </div>
+
+        </div>
+    </div>
 
 @endsection
